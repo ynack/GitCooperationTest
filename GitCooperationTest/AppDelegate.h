f57b9cbdc052990c91821705e17b45b8f762f0de
Yosuke Nakayama <nakayama.yosuke@gmail.com>
2022-07-20 17:16:48 +0900

    Initial Commit

---
+++ GitCooperationTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  GitCooperationTest
+//
+//  Created by yosuke on 2022/07/20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
